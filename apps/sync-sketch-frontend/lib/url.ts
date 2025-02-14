2b9ae9c1ed20d937d9749f9a7409fcb01e13e824
Anas Nayeem <anasnayeem97@gmail.com>
2025-02-15 00:09:02 +0530

    updated the url's

--- apps/sync-sketch-frontend/lib/url.ts
+++ apps/sync-sketch-frontend/lib/url.ts
@@ -1,2 +1,2 @@
-export const HTTP_BACKEND = "https://syncsketch.ezzcrafts.com/api"; 
+export const HTTP_BACKEND = "https://syncsketch.ezzcrafts.com"; 
 export const WS_BACKEND = "wss://syncsketch.ezzcrafts.com";
